c9eb75e54fa0010e7d5e31a02f86fa2dc16f1a06
Pavlos Fourfourianakis <ppfourfouria@gmail.com>
2024-07-29 08:24:55 +0300

    Update Brick.cpp

--- src/Brick.cpp
+++ src/Brick.cpp
@@ -5,6 +5,6 @@ void Brick::draw()
     if(isAlive)
     {
         DrawRectangleRounded(Rectangle{x,y,width+8.0f,height+8.0f},0.2f,4,color);
-        DrawRectangleRoundedLines(Rectangle{x,y,width+8.0f,height+8.0f},0.2f,4,2.0f,DARKGRAY);
+        DrawRectangleRoundedLines(Rectangle{x,y,width+8.0f,height+8.0f},0.2f,4,2.0f,Color{color.r,color.g,color.b,100});
     }
 }
