0f5bb432d7f50b54cffd927be4b574b525446154
Pavlos Fourfourianakis <ppfourfouria@gmail.com>
2024-07-29 08:25:28 +0300

    Update Brick.hpp

--- src/Brick.hpp
+++ src/Brick.hpp
@@ -1,14 +1,14 @@
 #pragma once
 #include <raylib.h>
 
-class Brick{
+class Brick
+{
+
     public:
 
-        //attributes
-        float x{0.0f},y{0.0f},width{40.0f},height{14.0f};
+        float x,y,width{40.0f},height{14.0f};
         bool isAlive{true};
-        Color color{WHITE};
+        Color color;
 
-        //methods
         void draw();
 };
