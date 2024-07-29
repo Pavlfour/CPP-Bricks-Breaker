dccd51dfdc65e353319ae56e7d96b65514004bfb
Pavlos Fourfourianakis <ppfourfouria@gmail.com>
2024-07-29 08:29:41 +0300

    Update Paddle.cpp

--- src/Paddle.cpp
+++ src/Paddle.cpp
@@ -5,20 +5,20 @@ void Paddle::draw()
     DrawRectangleRec({x,y,width,height},WHITE);
 }
 
-
-
 void Paddle::update()
 {
     if(IsKeyDown(KEY_LEFT))
     {
         x -= speed;
-        if(x <= 0.0f) //check if the paddle collides with the left side of the screen
+        //check if the paddle collides with the left side of the screen
+        if(x <= 0.0f)
             x = 0.0f;
     }
     else if(IsKeyDown(KEY_RIGHT))
     {
         x += speed;
-        if(x+80.0f >= float(GetScreenWidth())) //the same for the right side
-            x = float(GetScreenWidth()) - 80.0f ;
+        //the same for the right side
+        if(x+width >= float(GetScreenWidth()))
+            x = float(GetScreenWidth()) - width ;
     }
 }
